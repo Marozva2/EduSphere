59983d5befa053f63e4dcf03d7fb9e40eed3730e
Jerry Tarus <jerrytarus@gmail.com>
2024-02-17 14:33:21 +0300

    Completed all CRUD functions on faculty route
    I'll test the route later
    
    Later: Work on Lecturer blueprint

--- app/routes/faculty_bp.py
+++ app/routes/faculty_bp.py
@@ -10,9 +10,9 @@ api = Api(faculty_bp)
 
 # Request parsers for POST and PATCH requests
 post_args = reqparse.RequestParser()
-post_args.add_argument('id', type=str, required=True, help='Id is required')
+post_args.add_argument('id', type=str, required=True, help='ID is required')
 post_args.add_argument('name', type=str, required=True, help='name is required')
-post_args.add_argument('email', type=str, required=True, help='email code is required')
+post_args.add_argument('email', type=str, required=True, help='email is required')
 post_args.add_argument('department_id', type=int, required=True, help='Department is required')
 patch_args = reqparse.RequestParser()
 patch_args.add_argument('name', type=str)
@@ -23,3 +23,29 @@ patch_args.add_argument('department_id', type=int)
 faculty_schema = FacultySchema(many=True)
 faculty_schema_single = FacultySchema()
 
+
+class Faculties(Resource):
+    # GET request to retrieve all faculties
+    def get(self):
+        faculties = Faculty.query.all()
+        result = faculty_schema.dump(faculties)
+        return jsonify(result)
+
+    # POST request to create a new faculty
+    def post(self):
+        data = post_args.parse_args()
+
+        # Check if faculty with the same id already exists
+        faculties = Faculty.query.filter_by(id=data[id]).first()
+        if faculties:
+            abort(409, detail=f"Oups! Faculty with that id already exists")
+
+        # Create a new faculty instance and add it to the db
+        new_faculty = Faculty(
+            name=data['name'], email=data['email'], department_id=data['department_id'])
+        db.session.add(new_faculty)
+        db.session.commit()
+
+        # Serialize and return the new faculty
+        result = faculty_schema.dump(new_faculty)
+        return result, 201
