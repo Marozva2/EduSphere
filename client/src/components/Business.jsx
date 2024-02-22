f70d8a6cdf8214d6479581c3346d9e8e37dfcae7
michael <michaelngige@gmail.com>
2024-02-22 08:27:57 +0300

    [Head] [Business component fetching from endpoints]

--- client/src/components/Business.jsx
+++ client/src/components/Business.jsx
@@ -1,32 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom"; 
 
 function Business() {
+ 
+  const [courseWork, setCourseWork] = useState([]);
+  const [displayCourseWork, setDisplayCourseWork] = useState(false);
+  const fetchCourseWork = async () => {
+    try {
+      const response = await fetch("/course-works");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json(); 
+      setCourseWork(data); 
+      setDisplayCourseWork(true); 
+    } catch (error) {
+      console.error("Error fetching course work:", error);
+    }
+  };  
+  const handleEdusphereClick = () => {
+    fetchCourseWork(); 
+  };  
+  useEffect(() => {
+    fetchCourseWork(); 
+  }, []); 
+
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">Economics</Navbar.Brand>
+        <Container>         
+          <Nav.Link onClick={handleEdusphereClick}>
+            Edusphere School of Economics
+          </Nav.Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Edusphere School of Economics</Nav.Link>
-              <Nav.Link href="#link"></Nav.Link>
+              <Nav.Link href="#home">CourseWork</Nav.Link>
+              <Nav.Link href="#link"></Nav.Link>         
               <NavDropdown title="Courses" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Business Economics</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Human Resource
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                 Economics and Statistics
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                 Post-graduate Studies
-                </NavDropdown.Item>
+                {displayCourseWork &&
+                  courseWork.map((course) => (
+                    <NavDropdown.Item
+                      key={course.id}
+                      href={`#action/${course.id}`}
+                    >
+                      {course.course_name}
+                    </NavDropdown.Item>
+                  ))}
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
@@ -36,4 +60,4 @@ function Business() {
   );
 }
 
-export default Business
+export default Business;
