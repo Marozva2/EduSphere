07445013f5e202dec0dc28e8ca8aa236ae345591
Jerry Tarus <jerrytarus@gmail.com>
2024-02-23 14:43:25 +0300

    Feature: StudentNavbar
    Added Navbar for the student dashboard

---
+++ client/src/components/Dashboards/AdminNavbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const AdminNavbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
+      <div className="container-fluid">
+        <div className="navbar-wrapper">
+          <div className="navbar-toggle d-inline">
+            <button type="button" className="navbar-toggler">
+              <span className="navbar-toggler-bar bar1"></span>
+              <span className="navbar-toggler-bar bar2"></span>
+              <span className="navbar-toggler-bar bar3"></span>
+            </button>
+          </div>
+          <a className="navbar-brand" href="#pablo">Admin Dashboard</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default AdminNavbar;
