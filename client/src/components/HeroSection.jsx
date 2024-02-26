354969836b084bbcd1d576bcc8dfdfc73591ba36
Jerry Tarus <jerrytarus@gmail.com>
2024-02-26 07:50:39 +0300

    Feature: testimonials
    Styled and tweaked the testimonials section to look proper

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -137,48 +137,124 @@ function HeroSection() {
     </section>
 
     <section id="testimonials" className="testimonials section-bg">
-      <div className="container" data-aos="fade-up">
-        <div className="section-header">
-          <h2>Testimonials</h2>
-          <h3>What Are People <span>Saying About Us</span></h3>
-        </div>
+  <div className="container" data-aos="fade-up">
+    <div className="section-header">
+      <h2>Testimonials</h2>
+      <h3>What Are People <span>Saying About Us</span></h3>
+    </div>
 
-        <div className="slides-1 swiper" data-aos="fade-up" data-aos-delay="100">
-          <div className="swiper-wrapper">
+    <div className="testimonials-carousel swiper" data-aos="fade-up" data-aos-delay="100">
+      <div className="swiper-wrapper">
 
-            <div className="swiper-slide">
-              <div className="testimonial-item">
-                <div className="row gy-4 justify-content-center">
-                  <div className="col-lg-6">
-                    <div className="testimonial-content">
-                      <p>
-                        <i className="bi bi-quote quote-icon-left"></i>
-                        “The best florist in town, so many beautiful flowers to choose from”
-                        “Excellent service, will be back again”
-                        <i className="bi bi-quote quote-icon-right"></i>
-                      </p>
-                      <h3>Esteemed Customer</h3>
-                      <h4>Ceo &amp; Founder</h4>
-                      <div className="stars">
-                        <i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-2 text-center">
-                    <img src="assets/img/testimonials/testimonials-1.jpg" className="img-fluid testimonial-img" alt="testimonial-image1" />
+        <div className="swiper-slide">
+          <div className="testimonial-item">
+            <div className="row gy-4 justify-content-center">
+              <div className="col-lg-6">
+                <div className="testimonial-content">
+                  <p>
+                    <i className="bi bi-quote quote-icon-left"></i>
+                    “The best florist in town, so many beautiful flowers to choose from”
+                    “Excellent service, will be back again”
+                    <i className="bi bi-quote quote-icon-right"></i>
+                  </p>
+                  <h3>Esteemed Customer</h3>
+                  <h4>Ceo &amp; Founder</h4>
+                  <div className="stars">
+                    <i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i>
                   </div>
                 </div>
               </div>
+              <div className="col-lg-2 text-center">
+                <img src="../../images/testimonials-1.jpg" className="img-fluid testimonial-img" alt="testimonial-image1" />
+              </div>
             </div>
-
-            {/* Other testimonial items go here */}
-
           </div>
-          <div className="swiper-pagination"></div>
+        </div>
+
+        {/* Second testimonial */}
+        <div className="swiper-slide">
+          <div className="testimonial-item">
+            <div className="row gy-4 justify-content-center">
+              <div className="col-lg-6">
+                <div className="testimonial-content">
+                  <p>
+                    <i className="bi bi-quote quote-icon-left"></i>
+                    “The best florist in town, so many beautiful flowers to choose from”
+                    “Excellent service, will be back again”
+                    <i className="bi bi-quote quote-icon-right"></i>
+                  </p>
+                  <h3>Esteemed Customer</h3>
+                  <h4>Customer</h4>
+                  <div className="stars">
+                    <i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-2 text-center">
+                <img src="../../images/testimonials-2.jpg" className="img-fluid testimonial-img" alt="testimonial-image2" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Third testimonial */}
+        <div className="swiper-slide">
+          <div className="testimonial-item">
+            <div className="row gy-4 justify-content-center">
+              <div className="col-lg-6">
+                <div className="testimonial-content">
+                  <p>
+                    <i className="bi bi-quote quote-icon-left"></i>
+                    “The best florist in town, so many beautiful flowers to choose from”
+                    “Excellent service, will be back again”
+                    <i className="bi bi-quote quote-icon-right"></i>
+                  </p>
+                  <h3>Esteemed Customer</h3>
+                  <h4>Customer</h4>
+                  <div className="stars">
+                    <i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-2 text-center">
+                <img src="../../images/testimonials-3.jpg" className="img-fluid testimonial-img" alt="testimonial-image3" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+      
+        <div className="swiper-slide">
+          <div className="testimonial-item">
+            <div className="row gy-4 justify-content-center">
+              <div className="col-lg-6">
+                <div className="testimonial-content">
+                  <p>
+                    <i className="bi bi-quote quote-icon-left"></i>
+                    “The best florist in town, so many beautiful flowers to choose from”
+                    “Excellent service, will be back again”
+                    <i className="bi bi-quote quote-icon-right"></i>
+                  </p>
+                  <h3>Esteemed Customer</h3>
+                  <h4>Entrepreneur</h4>
+                  <div className="stars">
+                    <i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i><i className="bi bi-star-fill"></i>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-2 text-center">
+                <img src="../../images/testimonials-4.jpg" className="img-fluid testimonial-img" alt="testimonial-image4" />
+              </div>
+            </div>
+          </div>
         </div>
 
       </div>
-    </section>
+      <div className="swiper-pagination"></div>
+    </div>
+  </div>
+</section>
+
     
 </>
   );
